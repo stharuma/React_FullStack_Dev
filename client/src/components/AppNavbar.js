462b233a236d504a69b75f2cd3ca4d4e1750dc2b
stharuma <stharuma@gmail.com>
2018-07-16 01:18:18 -0400

    added Nav bar using bootstrap and reactstrap

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem ,
+  Container
+} from 'reactstrap';
+
+class AppNavbar extends Component{
+  // constructor(props) {
+  //    super(props);
+  //    this.toggle = this.toggle.bind(this);
+  //    this.state = {
+  //      isOpen: false
+  //    };
+  //  }
+  //  toggle() {
+  //    this.setState({
+  //      isOpen: !this.state.isOpen
+  //    });
+  //  }
+
+   state = {
+    isOpen: false
+   };
+
+   toggle = ()=> {
+     this.setState({
+       isOpen: !this.state.isOpen
+     });
+   }
+
+
+   render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">List Example</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="https://github.com/stharuma">GitHub</NavLink>
+            </NavItem>
+          {/*  <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown> */}
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+export default AppNavbar;
